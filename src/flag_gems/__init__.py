0f9ded430bb57a2abd1a82e92f89306ec721abfb
zhengyang <zhengyang@baai.ac.cn>
2024-06-26 15:51:38 +0800

    [Operator] Add allclose op

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -90,6 +90,7 @@ def enable(lib=aten_lib):
     lib.impl("outer", outer, "AutogradCUDA")
     lib.impl("cross_entropy_loss", cross_entropy_loss, "AutogradCUDA")
     lib.impl("isclose", isclose, "CUDA")
+    lib.impl("allclose", allclose, "CUDA")
 
 
 class use_gems:
